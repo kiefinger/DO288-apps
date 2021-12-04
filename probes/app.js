29cb953593ccaa66b3cb14af40a0240a0a8e7e48
ki <josef.kiefinger@gmx.de>
2021-12-04 16:56:18 +0100

    test

--- probes/app.js
+++ probes/app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
   app = express();
+  app = express();
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
